d82ccbdf5a93d681ce06797a31f7a382ec3420a6
maximmat <maximmatous@seznam.cz>
2022-06-20 13:42:14 +0200

    Přidejte soubory projektu.

---
+++ ciferace.cpp
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+
+int ciferace(int x) {
+	int m, vysledek = 0;
+	while (x > 0) {
+		m = x % 10;
+		vysledek = vysledek + m;
+		x = x / 10;
+	}
+
+	if (vysledek > 9) vysledek = ciferace(vysledek);
+
+	return vysledek;
+}
+
+int main() {
+	int vstup = 0;
+
+	printf("zadejte cislo: ");
+	scanf_s("%d", &vstup);
+	printf("Vysledek: %d", ciferace(vstup));
+}
